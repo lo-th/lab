c1017882542539aa40a14ecfb6cf455d2926983c
lo-th <3dflashlo@gmail.com>
2018-03-29 22:10:19 +0200

    up

--- build/lab.js
+++ build/lab.js
@@ -67582,7 +67582,7 @@ var intro = ( function () {
         document.body.appendChild( content );
 
         txt = document.createElement( 'div' );
-        txt.style.cssText = "text-align:center; position:absolute; margin:0; padding:0; top:50%; left:50%; width:300px; height:20px; margin-left:-150px; margin-top:110px; display:block; pointer-events:none; font-size: 12px;";
+        txt.style.cssText = "font-family: Monospace; color: #AAA; font-size: 12px; text-align:center; position:absolute; margin:0; padding:0; top:50%; left:50%; width:400px; height:20px; margin-left:-200px; margin-top:110px; display:block; pointer-events:none; font-size: 12px;";
         txt.textContent = text || 'loading...';
         document.body.appendChild( txt );
 
