f44f97c3bb921a19aee781dd1ef5826f0b9760c6
lo-th <3dflashlo@gmail.com>
2018-03-30 16:51:15 +0200

    update last three

--- src/externs.js
+++ src/externs.js
@@ -1,5 +1,5 @@
 var view, demos;
 var o3dgc;
 var process; 
-var THREE, WebGL2RenderingContext, TextDecoder, performance, ImageBitmap, createImageBitmap, fetch;
+var THREE, WebGL2RenderingContext, TextDecoder, performance, ImageBitmap, createImageBitmap, fetch, WeakMap;
 var CodeMirror, getComputedStyle, define, require, requirejs, esprima, exports, module;
--- src/externs_lab.js
+++ src/externs_lab.js
@@ -1,3 +1,3 @@
 var view, demos, editor;
 var process, exports, define, module; 
-var THREE, WebGL2RenderingContext, TextDecoder, performance, ImageBitmap, createImageBitmap, fetch;
+var THREE, WebGL2RenderingContext, TextDecoder, performance, ImageBitmap, createImageBitmap, fetch, WeakMap;
